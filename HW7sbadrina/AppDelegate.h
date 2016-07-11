9fdec0a3ed54fb1e59f95c4a7eca5a68e2e4b434
bshrinath <bshrinath@icloud.com>
2016-07-11 15:48:32 -0400

    Initial Commit

---
+++ HW7sbadrina/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HW7sbadrina
+//
+//  Created by Shrinath on 7/11/16.
+//  Copyright © 2016 cmu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HW7sbadrina/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  HW7sbadrina
+//
+//  Created by Shrinath on 7/11/16.
+//  Copyright © 2016 cmu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
